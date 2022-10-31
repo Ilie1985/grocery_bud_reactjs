69c85b0956fec2398cf347c6a62905fa89651a2e
Ilie1985 <ilie.marian32@yahoo.com>
2022-10-31 14:31:33 +0000

    setting the states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(true);
+
+  return <div className="App"></div>;
 }
 
 export default App;
