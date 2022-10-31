e32ae480dc59b86005a7f0b6614c722a82188157
Ilie1985 <ilie.marian32@yahoo.com>
2022-10-31 14:50:40 +0000

    jsx return

--- src/App.js
+++ src/App.js
@@ -5,9 +5,40 @@ import Alert from "./Alert";
 function App() {
   const [name, setName] = useState("");
   const [list, setList] = useState([]);
-  const [isEditing, setIsEditing] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, message: "", type: "" });
 
-  return <div className="App"></div>;
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={submitHandler}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <button className="submit-btn" type="submit">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear Items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
